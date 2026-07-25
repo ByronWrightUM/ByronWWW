ffbace4bdcbc5c94a54b22081fc1af0cede2b6ac
Byron Wright <byron.wright@umanitoba.ca>
2026-07-25 17:13:00 -0500

    add trailing slash to subsite

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ByronWrightUM.github.io',
-  base: '/ByronWWW',
+  base: '/ByronWWW/',
 });
