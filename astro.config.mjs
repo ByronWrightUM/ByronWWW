a1755c3b511a3a2542888038070c34310b7a3e20
Byron Wright <byron.wright@umanitoba.ca>
2026-08-20 09:39:04 -0500

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ByronWrightUM.github.io',
+  site: 'https://www.byronwright.ca',
   base: '/',
+  integrations: [sitemap()],
 });
